a364736f5d474634062e972743cf377601495d58
Sai Sreenivas <52418873+Sreenivas-2@users.noreply.github.com>
2020-05-03 15:26:17 +0530

    Add files via upload

--- SEM-6/LINEAR_OPTIMIZATION/ASSGN-1/p1.py
+++ SEM-6/LINEAR_OPTIMIZATION/ASSGN-1/p1.py
@@ -1,142 +1,120 @@
 import numpy as np
-import sympy as sp
-import math
-from numpy.random import randint
 import random
 
-A = np.array([[1,1,0],
-			 [0,-1,1],
-			 [-1,0,0],
-			 [0,-1,0],
-			 [0,0,-1]])
+def initExtreme(A, b):
+	'''
+		A => m*n matrix where each row consists of coefficients of the constraints
+		b => vector of length m, R.H.S values of the constraints
+		returns => Inital extreme point and the active set (indices of n linearly independent rows of A)
+	'''
 
-b = np.array([1,0,0,0,0])
-
-c = np.array([1,1,1])
-
-# A = np.array([[1,1,2],
-# 	 [-2,-2,-10],
-# 	 [-1,0,0],
-# 	 [0,-1,0],
-# 	 [0,0,-1],
-# 	 [0,0,1]])
-
-# b = np.array([2,-10,0,0,0,1])
-
-# c = np.array([0,0,1])
-
-# A = np.array([[1,1],
-# 	 [-2,-2],
-# 	 [-1,0],
-# 	 [0,-1]])
-
-# b = np.array([2,-10,0,0])
-
-# c = np.array([3,-1])
-
-
-def initExtreme(A, b, c):
-
-	b = b.reshape(-1, 1)
-	A_ = np.hstack((A, b))
-	add = np.zeros((2, A_.shape[1]))
-	add[0][-1] = -1
-	add[1][-1] = 1
-	A_ = np.vstack((A_, add))
-	b_ = np.vstack((b, np.array([0, 1]).reshape(-1,1)))
-	c_ = np.zeros(A_.shape[1])
+	c_ = np.zeros(A.shape[1] + 1)
 	c_[-1] = 1
+	x = np.zeros(A.shape[1] + 1)
+	x[-1] = np.min(b)
 
-	x = np.zeros(A_.shape[1])
-	x[-1] = 1
+	indices = []
+	for index, row in enumerate(b):
+		if row == 0:
+			arr = A[index]
+			if len(np.where(arr == 0.)[0]) == A.shape[1] - 1 and (arr[np.nonzero(arr)] == 1 or arr[np.nonzero(arr)] == -1):
+				indices.append(index)
 
-	J = np.arange(A_.shape[1])
-	while A_.shape[0] - 2 in J or np.linalg.det(A_[J,:]) == 0:
-		J = np.array(randint(0, A_.shape[0] - 1, A_.shape[1]))
-		# J = np.array(random.sample(range(0, A_.shape[0] - 1), 3))
+	ones = np.ones((A.shape[0], 1))
+	ones[np.array(indices), -1] = 0
+	A_ = np.hstack((A, ones))
+	add = np.zeros((2, A_.shape[1]))
+	add = np.zeros((1, A_.shape[1]))
+	add[0][-1] = 1
 
-	x = simplex(A_, b_, c_, x, J, False)
-	if x[-1] > 0:
+	A_ = np.vstack((A_, add))
+	b_ = np.vstack((b, np.array([0]).reshape(-1,1)))
+
+	indices.append(np.argmin(b))
+	J = np.array(indices)
+	
+	x, J = simplex(A_, b_, c_, x, J)
+
+	if x[-1] < 0:
 		return None, None
 
-	J = getActiveSet(A, b, x[:-1])
+	J = get_ActiveSet(A, b, x[:-1])
 
 	return x[:-1], J
 
 
-def getActiveSet(A, b, x):
+def get_ActiveSet(A, b, x):
 	'''
 		A => m*n matrix where each row consists of coefficients of the constraints
 		b => vector of length m, R.H.S values of the constraints
-		x => extreme point
-		returns => Active set at extreme point x
+		x => current extreme point
+		returns => Active set at extreme point x (indices of n linearly independent rows of A)
 	'''
 
 	x = x.reshape(-1,1)
-	M = (A.dot(x) - b).round(3)
-	arr = np.array(np.where(M == 0)[0])
+	M = A.dot(x) - b
+	arr = np.array(np.where((M >= -0.000000001) & (M <= 0.000000001))[0])
 	return arr
 
 
 
 
-def computeZ(A, c, J, flag):
+def get_InactiveIndex(A, c, J):
 	'''
 		A => m*n matrix where each row consists of coefficients of the constraints
 		c => coefficients in the objective function
-		J => active set at extreme point
-		returns => z
+		J => active set at current extreme point
+		returns => Index of the row present in the active set which shall be removed for obtaining next neighboring point.
 	'''
 
 	Aj = A[J, :]
 	c = c.reshape(-1,1)
 	z = np.linalg.inv(Aj.T).dot(-c)
 
-	if flag :
-		if (z <= 0).all():
-			return -1
-		else:
-			l = np.where(z>0)[0]
-			index = np.where(J == np.min(J[l]))
-			return index[0]
+	if (z <= 0).all():
+		return -1
 	else:
-		if (z >= 0).all():
-			return -1
-		else:
-			l = np.where(z<0)[0]
-			index = np.where(J == np.min(J[l]))
-			return index[0]
+		l = np.where(z>0)[0]
+		index = np.where(J == np.min(J[l]))
+		return index[0]
 
 
-def computeDeltaX(A, k, J):
+def get_DirVec(A, k, J):
 	'''
 		A => m*n matrix where each row consists of coefficients of the constraints
-		k => indix of element with z < 0
-		returns => deltaX
+		k => index of element with z < 0 (row which shall be removed from active set)
+		J => active set at current extreme point
+		returns => direction vector from current point to the neighboring point
 	'''
 
 	Aj = A[J, :]
 	rhs = np.zeros((Aj.shape[1],1))
 	rhs[k] = -1
-	deltaX = np.linalg.inv(Aj).dot(rhs)
-	return deltaX
+	DirVec = np.linalg.inv(Aj).dot(rhs)
+	return DirVec
 
 
-def getAlpha(A, b, x, deltaX, J):
+def get_Factor_and_Index(A, b, x, DirVec, J):
 	'''
+		x' = x + (alpha)*(DirVec)
+
 		A => m*n matrix where each row consists of coefficients of the constraints
 		b => vector of length m, R.H.S values of the constraints
-		returns => alpha
+		x => current extreme point
+		DirVec => direction vector from current point to the neighboring point
+		J => active set at the current extreme point
+		returns => alpha, index (index of new row which shall replace index obtainded from get_InactiveIndex function in the active set)
 	'''
 
-	k = A.dot(deltaX).round(4)
+	k = A.dot(DirVec)
 	index = np.array(np.where(k > 0)[0])
+	index = np.array(list(set(list(index)) - set(list(J))))
 	A = A[index, :]
 	b = b[index]
 	x = x.reshape(-1,1)
 	b = b.reshape(-1,1)
 	a = b - A.dot(x)
-	m = A.dot(deltaX)
+	m = A.dot(DirVec)
 	m.reshape(-1,1)
 	arr = np.divide(a, m)
 	alpha = np.min(arr)
@@ -144,40 +122,102 @@ def getAlpha(A, b, x, deltaX, J):
 	return alpha, index[ind]
 
 
-def simplex (A, b, c, x, J, flag = True):
+def simplex(A, b, c, x, J):
+	'''
+		Main Algorithm Implementation
 
-	k = computeZ(A, c, J, flag)
+		A => m*n matrix where each row consists of coefficients of the constraints
+		b => vector of length m, R.H.S values of the constraints
+		c => coefficients in the objective function
+		x => Initial extreme point
+		J => active set at the current extreme point
+		returns => x (solution), J (final active set)
+	'''
+
+	k = get_InactiveIndex(A, c, J)
 
 	cnt = 1
 	while k != -1:
 		print('Iteration - ' + str(cnt))
 		cnt += 1
-		deltaX = computeDeltaX(A, k, J)
-		deltaX = deltaX.reshape(-1,1)
-		if (A.dot(deltaX) <= 0).all():
-			return None
 
-		alpha, index = getAlpha(A, b, x, deltaX, J)
-		l = alpha*deltaX
+		DirVec = get_DirVec(A, k, J)
+		DirVec = DirVec.reshape(-1,1)
+		if (A.dot(DirVec) <= 0).all():
+			return None, None
+
+		alpha, index = get_Factor_and_Index(A, b, x, DirVec, J)
 		x = x.reshape(-1,1)
-		x = x + l
+		x = x + (alpha * DirVec)
 		J = np.delete(J, k)
 		J = np.append(J, index)
-		k = computeZ(A, c, J, flag)
+		k = get_InactiveIndex(A, c, J)
 
-	return x.round(4)
+	return x, J
+
+
+def main(A, b, c):
+
+	b = b.reshape(-1, 1)
+	if (b >= 0).all():
+		x = np.zeros((1, A.shape[1]))
+		J = get_ActiveSet(A, b, x)
+
+	else :
+		x, J = initExtreme(A, b)
+
+	if x is None:
+		return None, -1
+	else:
+		x = x.reshape(-1, 1)
+		c = c.reshape(-1, 1)
+		x, J = simplex(A, b, c, x, J)
+		if x is None:
+			return None, -2
+
+		else:
+			return x, J
+
 
 if __name__ == '__main__':
 
-	x, J = initExtreme(A, b, c)
-	if x is None:
-		print('Infeasible')
+	# Input Section
+	rows = int(input('Enter the number of constraints : '))
+	print('Enter the elements of the matrix A row wise with row elements space separated (same as a matrix form)')
+
+	A = []
+	for i in range(rows):
+	    a = list(map(float, input().split()))
+	    A.append(a)
+	A = np.array(A)
+
+	print('Enter the elements of b space separated')
+	b = list(map(float, input().split()))
+	b = np.array(b)
+
+	print('Enter the elements of c space separated')
+	c = list(map(float, input().split()))
+	c = np.array(c)
+
+	# Inputs Given
+	print('\nInputs Given :')
+	print('A\n', A)
+	print('b\n', b)
+	print('c\n', c)
+	print()
+
+	# Algorithm
+	x, J = main(A, b, c)
+	if x is not None:
+		x = x.round(3)
+		print('Solution :\n', x)
+		print('Maximum Value of c :', c.dot(x.reshape(-1,1)))
+
+	elif J == -1:
+		print('Infeasible Solution')
+
 	else:
-		x = simplex(A, b, c, x, J)
-		if x is None:
-			print('Unbounded')
-		else:
-			print(x)
+		print('Unbounded Solution')
 
 
 		
